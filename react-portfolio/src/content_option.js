d5030a6fb9ca0c981c0be65c8cd911cacd5759bc
eialid <Eaj@#!$567890>
2025-01-23 12:20:38 -0330

    full project without node modules

---
+++ react-portfolio/src/content_option.js
@@ -0,0 +1,127 @@
+const logotext = "EIALID JOY";
+const meta = {
+    title: "Eialid Ahmed Joy",
+    description: "I’m Eialid Ahmed Joy data scientist _ Full stack devloper,currently working in Berlin",
+};
+const WavingHand = () => {
+    return (
+      <span className="animate-wave inline-block">🤚🏻</span>
+    )
+  }
+const introdata = {
+    title: "I’m Eialid Ahmed Joy",
+    animated: {
+        first: "Think. Code. Create.",
+        second: "Learn. Build. Grow.",
+        third: "Dream. Develop. Deliver.",
+    },
+    description: "Full-Stack Software Developer with 3+ years of expertise in Java, Spring Boot, React, Python, and microservices",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "Professional Summary",
+    aboutme: "Full-Stack Software Engineer with 3 years of experience building robust applications using Java, Spring Boot, React, and Python. Skilled in REST API development, system design, and SQL. \n Experienced in Docker deployment, managing financial software projects, and implementing microservices architectures. Seeking a challenging role to leverage my technical expertise and contribute to innovative solutions.",
+};
+
+const resume = {
+    title: "Resume",
+    aboutme: "Full-Stack Software Engineer with 3 years of experience building robust applications using Java, Spring Boot, React, and Python. Skilled in REST API development, system design, and SQL. \n Experienced in Docker deployment, managing financial software projects, and implementing microservices architectures. Seeking a challenging role to leverage my technical expertise and contribute to innovative solutions.",
+};
+
+const worktimeline = [{
+        jobtitle: "Business Accelerate BD Limited",
+        where: "Software Engineer",
+        date: "2021 (November) - 2024 (February)",
+    },
+    {
+        jobtitle: "Business Accelerate BD Limited",
+        where: "Software Engineer Trainee",
+        date: "2021 (May) - 2021 (October)",
+    }
+];
+
+const skills = [{
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "JavaScript",
+        value: 81,
+    },
+    {
+        name: "Python",
+        value: 81,
+    },
+    {
+        name: "Spring Boot",
+        value: 84,
+    },
+    {
+        name: "React Js",
+        value: 75,
+    },
+    {
+        name: "Django",
+        value: 72,
+    },
+    {
+        name: "Microservices",
+        value: 70,
+    },
+];
+
+const dataportfolio = [{
+        img: "../Pictures/radar data viewer.webp",
+        description: "Radar Data Viewer",
+        link: "#",
+    },
+    {
+        img: "../Pictures/Todo List.png",
+        description: "Personal TODO",
+        link: "#",
+    },
+    {
+        img: "../Pictures/Movie Image.jpg",
+        description: "Movie Ratings using Microservices",
+        link: "#",
+    },
+    {
+        img: "../Pictures/Resume.webp",
+        description: "Personal Resume",
+        link: "#",
+    },
+    {
+        img: "../Pictures/Authentication.webp",
+        description: "Spring Authentication",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/EialidJoy",
+    facebook: "https://www.facebook.com/eialid.joy",
+    linkedin: "https://linkedin.com/in/eialid-ahmed-joy-127161130",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    resume,
+    skills,
+    introdata,
+    WavingHand,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
